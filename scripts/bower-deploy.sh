84bc5d5c11f3b2d0179ee7e1c7c1d24cbda789b0
Tom Vincent <git@tlvince.com>
2015-05-11 13:56:13 +0100

    fix(bower): build in npm context

--- scripts/bower-deploy.sh
+++ scripts/bower-deploy.sh
@@ -9,12 +9,11 @@ error() { info "$1"; exit 1; }
 [[ "$TRAVIS_PULL_REQUEST" == "false" ]] || error "Not deploying pull requests"
 
 git clone https://github.com/eHealthAfrica/qsl.js-bower.git deploy
-mv bower.json README.md LICENSE deploy
+mv bower.json README.md LICENSE dist/* deploy
 cd deploy
 git add .
 git config user.email "ehealthafrica-ci@tlvince.com"
 git config user.name "eHealth Africa CI"
-npm run build
 git commit --all --message "$TRAVIS_TAG"
 git tag "$TRAVIS_TAG"
 echo -e "machine github.com\n  login $CI_USER_TOKEN" >> ~/.netrc
